9eacec67643b2f9aed032684a4011c25107ed3c9
Julian Muñoz Velázquez <julian.munoz@applydigital.com>
2023-10-19 15:47:21 -0300

    Añadido y estilizado componente navbar

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,23 +1,40 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+
 import Home from '../Home'
 import MyAccount  from '../MyAccount'
 import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
+
+import Navbar from '../../Components/Navbar'
+
 import './App.css'
+
+// Se encapsula en una función para definir las rutas usando el hook useRoutes
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> },
+  ]);
+
+  return routes;
+
+}
 
 function App() {
 
   return (
-    
-    <div className='bg-red-100'>
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SignIn />
-    </div>
+
+    // Así se enlazan las rutas y se puede redirigir usando el AppRoute
+    <BrowserRouter>
+      <AppRoutes></AppRoutes>
+      <Navbar/>
+    </BrowserRouter>
     
   )
 }
