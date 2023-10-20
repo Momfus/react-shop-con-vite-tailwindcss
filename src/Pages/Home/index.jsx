6dd23cb20af8c0976245c0009ca51d1437358dca
Julian Muñoz Velázquez <julian.munoz@applydigital.com>
2023-10-20 16:24:01 -0300

    Añadido uso de api para cards

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,37 @@
+import { useState, useEffect } from "react"; // useEffect = siempre que se usen API y comunicarse con la misma
+
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
 
+import apiUrl from "../../api";
+
 function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    // Método temporal para usar async
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/products`);
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        console.error(`Ocurrio un error: ${error}`);
+      }
+    };
+
+    // Usar la función para obtener los datos
+    fetchData();
+  }, []);
+
   return (
     <Layout>
       Home
-      <Card />
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {items?.map((item) => (
+          <Card key={item.id} data={item} />
+        ))}
+      </div>
     </Layout>
   );
 }
